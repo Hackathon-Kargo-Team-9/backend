a9bd3a556c2e4a3fa07551f0b5ebff06cf719580
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2022-08-13 12:02:54 +0700

    add truck urls to project setting

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include               
-from rest_framework import routers
+from rest_framework import routers             
+from management import views 
+
+router = routers.DefaultRouter()                   
+router.register(r'truck', views.TruckView, 'truck')  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),             
+    path('admin/', admin.site.urls),   
+    path('truck/', include(router.urls))          
 ]
