2acf83915372bf343391a70396558b3a75724b79
damarwardoyo-tkp <damar.wardoyo@tokopedia.com>
2022-08-13 15:56:09 +0700

    init driver module

--- backend/urls.py
+++ backend/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include               
-from rest_framework import routers             
-from management import views 
+from django.urls import path,include
+from rest_framework import routers
+
+from management import views
 
 router = routers.DefaultRouter()                   
-router.register(r'truck', views.TruckView, 'truck')      
-router.register(r'userz', views.UserzView, 'userz') 
+router.register(r'truck', views.TruckView, 'truck')
+router.register(r'userz', views.UserzView, 'userz')
+router.register(r'driver', views.DriverView, 'driver')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),   
-    path('', include(router.urls))          
+    path('', include(router.urls)),
 ]
