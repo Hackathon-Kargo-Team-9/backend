3b1540a1bb89c94e7b4bfe678df36bf9c38d77b2
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2022-08-13 15:01:46 +0700

    add user to admin and serializerz in management app

--- management/admin.py
+++ management/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Truck, 
+from .models import Truck, User
 
 admin.site.register(Truck)
-admin.site.register(Truck)
+admin.site.register(User)
--- management/serializers.py
+++ management/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Truck
+from .models import Truck, User
 
 class TruckSerializer(serializers.ModelSerializer):
     class Meta:
         model = Truck
         fields = ('id' ,'plate_number', 'plate_type', 'truck_type', 'production_year', 'status')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id' ,'name', 'role')
