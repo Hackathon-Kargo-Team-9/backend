f2a07def79cdfe3a18e06266368c2b2f29b0546b
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2022-08-13 14:32:25 +0700

    add status to truck model in management app

--- management/serializers.py
+++ management/serializers.py
@@ -4,4 +4,4 @@ from .models import Truck
 class TruckSerializer(serializers.ModelSerializer):
     class Meta:
         model = Truck
-        fields = ('id' ,'plate_number', 'plate_type', 'truck_type', 'production_year')
+        fields = ('id' ,'plate_number', 'plate_type', 'truck_type', 'production_year', 'status')
