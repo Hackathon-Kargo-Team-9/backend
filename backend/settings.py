08b810ba0e7a41ab34358bbd6ecb7c5f57a7c0ca
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2022-08-13 14:46:16 +0700

    fix cors

--- backend/settings.py
+++ backend/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
