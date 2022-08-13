2cb70c9edb962b7d106ca1bd201fc80280105c91
Irfan Maulana Nasution <irfanmaulananasution@gmail.com>
2022-08-13 14:09:09 +0700

    truck finishing

--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,7 @@ from rest_framework import routers
 from management import views 
 
 router = routers.DefaultRouter()                   
-router.register(r'truck', views.TruckView, 'truck')  
+router.register(r'', views.TruckView, 'truck')  
 
 urlpatterns = [
     path('admin/', admin.site.urls),   
